3d3a5cabe58dd8a457339fbb28ce7d5004f90858
Arnaldo <arnaldosepulveda@gmail.com>
2024-03-17 11:43:20 -0300

    edit route server.js

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// Wildcard route to direct users to a Index page
+app.get('*', (req, res) =>
+	res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
 // GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
